ee9d78821dcf4cbc915a505d6e9ae96fc62a4cfc
Paymon <paymon@geeks.ltd.uk>
2017-08-16 14:30:57 +0100

    Fixed formatting

--- Shared/Annotation.cs
+++ Shared/Annotation.cs
@@ -2,7 +2,6 @@ namespace Zebble
 {
     using System;
     using System.Threading.Tasks;
-    using Zebble;
     using Zebble.Services;
 
     partial class Map
@@ -10,65 +9,35 @@ namespace Zebble
         public partial class Annotation : IDisposable
         {
             public readonly AsyncEvent Tapped = new AsyncEvent();
-            internal void RaiseTapped() => Tapped.RaiseOn(Device.ThreadPool);
-            public string Title
-            {
-                get;
-                set;
-            }
 
-            = string.Empty;
+            internal void RaiseTapped() => Tapped.RaiseOn(Device.ThreadPool);
+
+            public string Title { get; set; } = string.Empty;
+
             /// <summary>
             /// This is ignored in UWP.
             /// </summary>
-            public string Subtitle
-            {
-                get;
-                set;
-            }
+            public string Subtitle { get; set; } = string.Empty;
 
-            = string.Empty;
-            public GeoLocation Location
-            {
-                get;
-                set;
-            }
+            public GeoLocation Location { get; set; } = new GeoLocation();
 
-            = new GeoLocation();
-            public object Native
-            {
-                get;
-                internal set;
-            }
+            public object Native { get; internal set; }
 
             public void Dispose() => Tapped.Dispose();
+
             /// <summary>
             /// Path to the pin icon (optional).
             /// </summary>
-            public string IconPath
-            {
-                get;
-                set;
-            }
+            public string IconPath { get; set; } = string.Empty;
 
-            = string.Empty;
-            public float IconWidth
-            {
-                get;
-                set;
-            }
+            public float IconWidth { get; set; } = 40;
 
-            = 40;
-            public float IconHeight
-            {
-                get;
-                set;
-            }
+            public float IconHeight { get; set; } = 60;
 
-            = 60;
             internal async Task<ImageService.ImageProvider> GetPinImageProvider()
             {
-                var result = ImageService.GetImageProvider(IconPath, new Size(IconWidth, IconHeight), Stretch.Fit);
+                var result = ImageService.GetImageProvider(IconPath, new Size(IconWidth, IconHeight),
+                    Stretch.Fit);
                 await result.Result();
                 return result;
             }
