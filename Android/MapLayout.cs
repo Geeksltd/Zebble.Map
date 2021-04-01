120e86abc96918dddf215d172d4eb7411ffb8cf5
Mohammad Sadegh Zojaji <ms.zojaji@gmail.com>
2021-04-01 19:51:39 +0430

    Fix MaplLayout touch issue

--- Android/MapLayout.cs
+++ Android/MapLayout.cs
@@ -3,51 +3,19 @@
     using Android.Content;
     using Android.Views;
     using Android.Widget;
+    using System;
+    using Zebble.AndroidOS;
 
     class MapLayout : FrameLayout
     {
-        ScrollView ScrollView;
+        FrameLayout OuterView;
 
         public MapLayout(Context context) : base(context) { }
 
-        public override bool DispatchTouchEvent(MotionEvent ev)
+        public override bool OnInterceptTouchEvent(MotionEvent ev)
         {
-            FindScrollView();
-
-            var nativeScrollView = ScrollView?.Native as Android.Widget.ScrollView;
-
-            switch (ev.Action)
-            {
-                case MotionEventActions.Up:
-                case MotionEventActions.Down:
-                    if (nativeScrollView == null) break;
-
-                    nativeScrollView.RequestDisallowInterceptTouchEvent(disallowIntercept: true);
-                    ScrollView.EnableScrolling = false;
-
-                    break;
-            }
-
-            return base.DispatchTouchEvent(ev);
-        }
-
-        void FindScrollView()
-        {
-            if (ScrollView != null) return;
-
-            foreach (var currentPage in UIRuntime.PageContainer.AllChildren.OfType<Page>())
-            {
-                foreach (var pageChild in currentPage.AllChildren)
-                {
-                    if (pageChild is Canvas scrollWrapper && scrollWrapper.Id != null && scrollWrapper.Id == "BodyScrollerWrapper")
-                    {
-                        ScrollView = scrollWrapper.AllChildren.FirstOrDefault() as ScrollView;
-                        break;
-                    }
-                }
-
-                if (ScrollView != null) break;
-            }
+            this.TraverseUpToFind<AndroidGestureView>()?.OnTouchEvent(ev);
+            return base.OnInterceptTouchEvent(ev);
         }
     }
 }
